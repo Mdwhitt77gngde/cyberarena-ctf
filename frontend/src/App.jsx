70279ad85c00ec0cd7210fbeead186c8585542d5
Mdwhitt77gngde <u585507859@byui.edu>
2026-06-05 23:07:42 -0500

    Added ProtectedRoute to App.jsx, install Axios

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import ProtectedRoute from './components/ProtectedRoute'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Challenges from './pages/Challenges'
 import ChallengeDetail from './pages/ChallengeDetail'
 import Leaderboard from './pages/Leaderboard'
+import Dashboard from './pages/Dashboard'
 
 function App() {
   return (
@@ -18,9 +20,12 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/challenges" element={<Challenges />} />
-          <Route path="/challenges/:id" element={<ChallengeDetail />} />
-          <Route path="/leaderboard" element={<Leaderboard />} />
+          <Route element={<ProtectedRoute />}>
+            <Route path="/challenges" element={<Challenges />} />
+            <Route path="/challenges/:id" element={<ChallengeDetail />} />
+            <Route path="/leaderboard" element={<Leaderboard />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Route>
         </Routes>
         <Footer />
       </BrowserRouter>
